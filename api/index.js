ae54049f82c8b6e28e972460cf4f604f4e9db501
zodean123 <dipanmallick7085@gmail.com>
2024-07-31 23:24:47 +0530

    updated index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ require('dotenv').config()
 
 app.use(cors({
   origin:process.env.FRONTEND_URL,
-  credentials: true,
+  credentials:true,
 }));
 
 
@@ -27,14 +27,22 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Failed to connect to MongoDB', err);
-});
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch(err => {
+//   console.error('Failed to connect to MongoDB', err);
+// });
+
+mongoose.connect('mongodb+srv://dipanmallick7085:dipanb660b6@cluster0.8oejhss.mongodb.net/');
+
+
+app.get("/", (req,res) =>{
+  res.send("Backend is Running.");
+})
+
 
 app.post('/register',async (req,res)=>{
     try{
