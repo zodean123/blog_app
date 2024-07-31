c44fcaa85c91adfbb41cce3a08934ff1c8297042
zodean123 <dipanmallick7085@gmail.com>
2024-07-31 23:05:31 +0530

    updated index.js

--- api/index.js
+++ api/index.js
@@ -27,7 +27,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'));
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
 
 app.post('/register',async (req,res)=>{
     try{
